f5ea3c3e7cbd8e551b3c3a6716c7d7b092e435f0
sharon002 <sharonjep2016@gmail.com>
2021-07-06 18:32:36 +0300

    add setting import

--- gallery/settings.py
+++ gallery/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
-
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -44,9 +43,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
-
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
