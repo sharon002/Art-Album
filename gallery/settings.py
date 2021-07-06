27cf37bc65b59a87a6f324082e9903c6fcad1ee3
sharon002 <sharonjep2016@gmail.com>
2021-07-06 17:50:00 +0300

    add installed app

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
+import django_herokusharalbum
 import dj_database_url
 from decouple import config,Csv
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'photos.apps.PhotosConfig',
     'bootstrap3',
+    'photos',
 ]
 
 MIDDLEWARE = [
